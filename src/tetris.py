c1e635d97a0bf3d24b587e52acc0a1d679b4f23c
Selina <selinakim343@outlook.com>
2022-10-10 04:54:35 -0400

    Creat frame of gameplay area
    Creat sprite for cubes that will be used to make the tetris figures
    Clean up structure of project

--- src/tetris.py
+++ src/tetris.py
@@ -1,6 +1,7 @@
 import pyglet
 from pyglet import image
 import random
+import os
 
 '''
 class Figure():
@@ -9,16 +10,69 @@ class Figure():
         # self.type =
 '''
 
-window = pyglet.window.Window(900
-                              ,700
+# Constants
+#------------------------------------------
+
+WINDOW_X = 900
+WINDOW_Y = 700
+CUBE_LENGTH = 32
+
+#------------------------------------------
+
+window = pyglet.window.Window(WINDOW_X
+                              ,WINDOW_Y
                               ,caption='PYTHON TETRIS GAME'
                               # ,resizable=True
                               )
+
+
+
+cubes = image.load('resources/img/cubes.png')
+cubesSeq = pyglet.image.ImageGrid(cubes, 1, 7)
+
+cubeRed = cubesSeq[0]
+cubeOrange = cubesSeq[1]
+cubeYellow = cubesSeq[2]
+cubeGreen = cubesSeq[3]
+cubeBlue = cubesSeq[4]
+cubePurple = cubesSeq[5]
+cubeGrey = cubesSeq[6]
+
+
+# Create the grey frame outline for game
+
+frameBatch = pyglet.graphics.Batch()
+
+def generateFrame(height, batch=None):
+    frame = []
+    for x in range(6):
+        for y in range(height):
+            if (y == 0) | (x == 5) | (y == height-1):
+                xPosLeft = (WINDOW_X//2) - (CUBE_LENGTH * (x + 1))
+                xPosRight = (WINDOW_X//2) + (CUBE_LENGTH * x)
+                yPos = CUBE_LENGTH * y
+                # print("block at x=" + str(xPosLeft) + ", y=" + str(yPos) + " and x=" + str(xPosRight) + ", y=" + str(yPos))
+                frame.append(
+                    pyglet.sprite.Sprite(img=cubeGrey
+                                        , x=xPosLeft
+                                        , y=yPos
+                                        , batch=batch))
+                frame.append(
+                    pyglet.sprite.Sprite(img=cubeGrey
+                                        , x=xPosRight
+                                        , y=yPos
+                                        , batch=batch))
+    return frame
+
+gameFrame = generateFrame(22, frameBatch)
+
+#------------------------------------------
 
 pic = image.load('ref/1200px-Typical_Tetris_Game.svg.png')
 pic.anchor_x = pic.width // 2
 pic.anchor_y = pic.height // 2
 picSprite = pyglet.sprite.Sprite(pic)
+picSprite.opacity = 50
 picSprite.scale = 700 / 2200
 picSprite.position = (window.width//2, window.height//2)
 
@@ -32,8 +86,8 @@ label = pyglet.text.Label('Score',
 def on_draw():
     window.clear()
     picSprite.draw()
+    frameBatch.draw()
     label.draw()
-    
 
 
 pyglet.app.run()
